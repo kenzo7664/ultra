fcf277940f996540e82688247da75bbfe2eba342
kenzo7664 <olaotanemmanuel1@gmail.com>
2021-10-18 16:18:47 +0100

    first commit

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,87 @@
+import React,{useState, useEffect} from 'react'
+import {FaTimes, FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon,
+NavMenu,
+NavItem,
+NavLinks,
+NavItemBtn,
+NavBtnLink} from './Navbar.elements'
+import { IconContext } from 'react-icons/lib'
+import {Button} from '../../globalStyles'
+
+
+
+
+
+const NavBar = () => {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+
+    const handleClick = () => setClick(!click)
+
+    const showButton = ()=>{
+        if(window.innerWidth <= 960){
+            setButton(false)
+        }else{
+            setButton(true)
+        }
+    }
+    useEffect(() => {
+       showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton)
+    return (
+        <>
+        <IconContext.Provider value = {{color:'#fff'}}>
+            <Nav>
+                <NavbarContainer>
+                  <NavLogo to ='/'>
+                      <NavIcon/>
+                      ULTRA
+                  </NavLogo>
+                  <MobileIcon onClick = {handleClick}>
+                      {click ? <FaTimes /> : <FaBars />}
+                  </MobileIcon>
+                  <NavMenu onClick ={handleClick} click = {click}>
+                      <NavItem>
+                          <NavLinks to= '/'>
+                              Home
+                          </NavLinks>
+                      </NavItem>
+                  
+                  
+                      <NavItem>
+                          <NavLinks to= '/services'>
+                              Services
+                          </NavLinks>
+                      </NavItem>
+                 
+                      <NavItem>
+                          <NavLinks to= '/products'>
+                              Products
+                          </NavLinks>
+                      </NavItem>
+                      <NavItemBtn>
+                          {button ? (
+                              <NavBtnLink to ='/sign-up'>
+                                 <Button primary>SIGN UP</Button>
+                              </NavBtnLink>
+
+
+                          ): (
+                              <NavBtnLink>
+                                  <Button  fontBig primary>SIGN UP</Button>
+                              </NavBtnLink>
+                          )} 
+                      </NavItemBtn>
+                  </NavMenu>
+
+                </NavbarContainer>
+            </Nav>
+        </IconContext.Provider>
+        </>
+    )
+}
+
+export default NavBar
